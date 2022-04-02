cc06c665e72699d6bf35a950b407cc51815ab909
MuraliKavi <pnbabu71@gmail.com>
2022-04-02 11:54:28 +0530

    new project

---
+++ src/org/test/selenium/Test.java
@@ -0,0 +1,28 @@
+package org.test.selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\JAI KUMAR\\eclipse-workspace\\Navigate\\Driver\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.navigate().to("https://www.facebook.com/login/");
+		
+		WebElement btnforgate = driver.findElement(By.xpath("//a[@class='_97w4']"));
+		btnforgate.click();
+		
+		driver.navigate().back();
+		
+	    //driver.navigate().forward();
+		
+		//driver.navigate().refresh();
+		
+		
+		
+	}
+
+}
